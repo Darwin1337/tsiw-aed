0e2ef5ffd6b4981e05a984de020e0b86ee335612
Diogo Borges <40754665+Darwin1337@users.noreply.github.com>
2020-10-19 16:47:15 +0100

    added exercise

--- AED - Ficha 03/Ex03.py
+++ AED - Ficha 03/Ex03.py
@@ -1,8 +1,18 @@
 import os
 import traceback
 
-# [Ficha 03 - Ex. 02] - Lê 10 números e verifica qual deles o maior
+# [Ficha 03 - Ex. 03] - Determina o fatorial de um número
 try:
+    isNumCorrect = False
+    while not isNumCorrect:
+        try:
+            num = int(input("Introduza um número: "))
+            if num >= 0: isNumCorrect = True
+        except:
+            continue
+    for x in range(num - 1, 0, -1):
+        num *= int(x)
+    print("Fatorial: " + str(num)) if num > 0 else print("Fatorial: 1")
 except Exception as e:
     print(str(e))
     traceback.print_exc()
