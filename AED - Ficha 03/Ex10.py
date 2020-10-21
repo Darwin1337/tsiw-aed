93fc492780cc058d7036cd8766067e7021f336c1
Diogo Borges <40754665+Darwin1337@users.noreply.github.com>
2020-10-21 14:44:57 +0100

    updated exercise

--- AED - Ficha 03/Ex10.py
+++ AED - Ficha 03/Ex10.py
@@ -1,21 +1,21 @@
 import os
 import traceback
 
-# [Ficha 03 - Ex. 10] - Converte número decimal em binário
+# [Ficha 03 - Ex. 10] - Converte um número decimal em binário
 
 try:
     isNumberCorrect, binario = False, ""
     while not isNumberCorrect:
         try:
             num = int(input("Introduza um número: "))
-            if num >= 0:
+            if num >= 1 and num <= 99:
                 isNumberCorrect = True
         except:
             continue
     while num >= 1:
         binario += str(int(num % 2))
         num /= 2
-    print("Binário: " + binario[::-1]) if num > 0 else print("Binário: 0")
+    print("Binário: " + binario[::-1])
 except Exception as e:
     print(str(e))
     traceback.print_exc()
